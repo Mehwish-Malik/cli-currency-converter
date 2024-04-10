86df45af0cede27a1e8c3f1f4b8a6fc543f110f3
wish Malik <malik.mehwish0078@gmail.com>
2024-04-10 15:31:04 -0700

    Created a Cli Based Currency Converter

---
+++ main.ts
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+const currency :any=(
+
+ {
+    USD:1, //Base currency
+    EUR:0.91,
+   DIN :904,
+    AED:75,
+    INR:74.57,
+    PKR:280,
+
+}
+
+);
+let user_Ans = await inquirer.prompt(
+    [
+    {
+    name:'from',
+    message:'Select Your Currency which you want to Convert',
+    type: 'list',
+    choices:['USD' , 'EUR' , 'DIN', 'AED' , 'INR','PKR']
+},
+
+{
+    name:'to',
+    message:'Select Your Currency in  which you want to Convert',
+    type:'list',
+    choices:['USD' , 'EUR' , 'DIN', 'AED' , 'INR','PKR']
+},
+
+
+
+{
+    name:'amount',
+    
+    message:'How much  Amount You Want to Convert',
+    type: 'number',
+}
+    ]
+)
+let fromAmount = currency [user_Ans.from];
+let toAmount = currency [user_Ans.to];
+ let amount = user_Ans.amount;
+
+ let baseAmount =amount / fromAmount; //usd base currency
+                  
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
